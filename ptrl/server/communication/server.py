acc73175fd07364560af3439d3ea58f8c17313fb
t9orhon <t9orhon@enib.fr>
2024-08-03 21:05:19 +0200

    update assign id

--- ptrl/server/communication/server.py
+++ ptrl/server/communication/server.py
@@ -9,6 +9,11 @@ class Server:
         self.server_socket.bind((host, port))
         self.server_socket.listen(5)
         print(f"Server listening on {host}:{port}")
+        self.data_settings = None
+        self.data_get = None
+        self.open_file_get()
+        self.open_file_settings()
+        self.start_json_updater()
 
     def start(self):
         try:
@@ -16,10 +21,12 @@ class Server:
                 conn, address = self.server_socket.accept()
                 print('Connected by', address)
                 id_ = self.receive_conn(conn)
+                self.send_id(conn, id_)
                 my_thread = ThreadClient(conn, id_)
                 my_thread.start()
                 self.active_threads.append(my_thread)
                 self.clean_threads()
+                self.handle_json_settings()
         except KeyboardInterrupt:
             print("Server shutting down...")
         finally:
@@ -37,5 +44,52 @@ class Server:
         if prefix == b'ID  ':
             id_ = int.from_bytes(conn.recv(1), 'big')
         if id_ == 0:
-            id_ = 1
+            self.data_settings["nb_player"] += 1
+            id_ = self.data_settings["nb_player"]
         return id_
+
+    def open_file_get(self):
+        try:
+            with open("game/data_json/data_get.json", 'r') as f:
+                self.data_get = json.load(f)
+        except:
+            print("error")
+            pass
+    
+    def open_file_settings(self):
+        try:
+            with open("game/data_json/settings.json", 'r') as f:
+                self.data_settings = json.load(f)
+        except:
+            print("error")
+            pass
+    
+    def handle_json_data(self):
+        while True:
+            try :
+                with open(f'game/data_json/data_get.json', 'r+', encoding='utf-8') as f:
+                    f.seek(0)
+                    json.dump(self.data_get, f, indent=4)
+                    f.truncate()
+            except:
+                print("error")
+                pass
+
+    def handle_json_settings(self):
+            try :
+                with open(f'game/data_json/settings.json', 'r+', encoding='utf-8') as f:
+                    f.seek(0)
+                    json.dump(self.data_settings, f, indent=4)
+                    f.truncate()
+            except:
+                print("error")
+                pass
+    
+    def start_json_updater(self):
+        """Start a separate thread to update JSON data."""
+        updater_thread = threading.Thread(target=self.handle_json_data, daemon=True)
+        updater_thread.start()
+    
+    def send_id(self, conn, id_):
+        id_prefix = b'ID  '
+        conn.sendall(id_prefix + id_.to_bytes())
